7c807b1cdacd7d264b946173f9493e6569e4c9c3
Matheus Lima <matheus.limastack@outlook.com>
2022-10-19 00:11:12 -0300

    add plugin folder and vim plugin bang command

---
+++ lua/spongebob/stupid.lua
@@ -0,0 +1,21 @@
+local vim = vim
+local system = vim.fn.system
+
+local M = {}
+
+function M.format (s, e)
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local str = vim.api.nvim_buf_get_lines(bufnr, s-1, e, true)
+  local new_str = ""
+  for i = 1, #str do
+      local c = str:sub(i,i)
+      if str:find(c)&1 == 1 then
+        new_str = new_str .. c
+      else 
+        new_str = new_str .. c:gsub("^%l", string.upper)
+      end
+  end
+end
+
+return M
