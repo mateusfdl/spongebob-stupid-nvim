ef9b8ffd767d61877b69aaf881385cf272c10028
Matheus Lima <matheus.limastack@outlook.com>
2022-10-19 01:14:37 -0300

    fix replace command

--- lua/spongebob/stupid.lua
+++ lua/spongebob/stupid.lua
@@ -10,14 +10,14 @@ function M.format (s, e)
   local new_str = ""
   for i = 1, #str do
       local c = str:sub(i,i)
-      if str:find(c)%1 == 1 then
+      if str:find(c)%2 == 1 then
         new_str = new_str .. c
       else
         new_str = new_str .. c:gsub("^%l", string.upper)
       end
   end
 
-  return vim.cmd('/' .. str .. '/' .. new_str)
+  return vim.cmd('s/' .. str .. '/' .. new_str)
 end
 
 return M
