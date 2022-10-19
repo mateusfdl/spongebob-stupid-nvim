8ea3c531c3e9035aea26015b12489968f38b63e7
Matheus Lima <matheus.limastack@outlook.com>
2022-10-19 00:55:25 -0300

    add vim cmd to replace text

--- lua/spongebob/stupid.lua
+++ lua/spongebob/stupid.lua
@@ -7,15 +7,18 @@ function M.format (s, e)
   local bufnr = vim.api.nvim_get_current_buf()
 
   local str = vim.api.nvim_buf_get_lines(bufnr, s-1, e, true)
-  local new_str = ""
+  str = "foo"
+  new_str = ""
   for i = 1, #str do
       local c = str:sub(i,i)
-      if str:find(c)&1 == 1 then
+      if str:find(c)%1 == 1 then
         new_str = new_str .. c
-      else 
+      else
         new_str = new_str .. c:gsub("^%l", string.upper)
       end
   end
+
+  return vim.cmd('/' .. str .. '/' .. new_str)
 end
 
 return M
